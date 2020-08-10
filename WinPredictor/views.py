edbc8ae1673e5583fdd34e4d0896c876194ae893
the-himanshu <56024176+the-himanshu@users.noreply.github.com>
2020-08-10 10:04:12 +0530

    Update views.py

--- WinPredictor/views.py
+++ WinPredictor/views.py
@@ -13,34 +13,36 @@ d2 = {'RCB':'Royal Challengers Bangalore', 'KKR':'Kolkata Knight Riders', 'CSK':
 x_temp = pd.read_csv('site_data.csv')
 model = pickle.load(open('Tfile.pkl', 'rb'))
 scaler = pickle.load(open('scaler.pkl', 'rb'))
-cols = ['batting_team', 'bowling_team', 'wicket_fallen', 'score', 'target', 'runs_prev_5', 'wick_prev_5' , 'balls_left', 'venue']
+cols = ['batting_team', 'bowling_team', 'wicket_fallen', 'score', 'target', 'runs_prev_5', 'wick_prev_5' , 'balls_left', 'venue', 'req']
 
 # Create your views here.
 def index(request) :
-    return render(request, 'winpredictor/home.html')
+    return render(request, 'winpredictor\home.html')
 
 def predict(request) :
-    name = query=request.GET.get('username','')
-    batting = query=request.GET.get('battingTeam','')
-    fielding = query=request.GET.get('fieldingTeam','')
-    venue = query=request.GET.get('venue','')
-    score = query=request.GET.get('score',0)
-    wickets = query=request.GET.get('wickets',0)
-    balls = query=request.GET.get('balls',0)
-    target = query=request.GET.get('target',0)
-    score5 = query=request.GET.get('score5',0)
-    wickets5 = query=request.GET.get('wickets5',0)
+    name = request.GET.get('username','')
+    batting = request.GET.get('battingTeam','')
+    fielding = request.GET.get('fieldingTeam','')
+    venue = request.GET.get('venue','')
+    score = request.GET.get('score',0)
+    wickets = request.GET.get('wickets',0)
+    balls = request.GET.get('balls',0)
+    target = request.GET.get('target',0)
+    score5 = request.GET.get('score5',0)
+    wickets5 = request.GET.get('wickets5',0)
+    req = int(target)-int(score)
 
     batting = d[batting]
     fielding = d[fielding]
 
-    data = [[batting, fielding, int(wickets), int(score), int(target), int(score5), int(wickets5), int(balls), venue]]
+    data = [[batting, fielding, int(wickets), int(score), int(target), int(score5), int(wickets5), int(balls), venue, int(req)]]
     p =  pd.DataFrame(data, columns = cols)
     x_temp_2 = pd.concat([x_temp, p])
     x_temp_2 = x_temp_2.iloc[:, 1:]
     x = pd.get_dummies(x_temp_2)
     x = pd.DataFrame(scaler.fit_transform(x), columns = x.columns)
     p = x.iloc[-1,:].values
+    print(p)
     x = model.predict([p])[0]
     y = model.predict_proba([p])
     if x==0 :
